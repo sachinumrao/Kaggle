18c5ae750933a765e09a3b6d5bd20efed8bdf194
Sachin Umrao <sachin.umrao.1512@gmail.com>
2020-06-23 03:58:13 +0530

    added hash vectorizer for data

---
+++ real_or_not/hashing_vectorizer.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import HashingVectorizer
+
+
+def get_hash_vectorizer(fname, n=20):
+    dfx = pd.read_csv(fname)
+    sentence_list = [sent for sent in dfx['clean_text'].values]
+    hasher = HashingVectorizer(n_features=n)
+    hasher.fit(sentence_list)
+    return hasher
+    pass
+
+
+def get_hash_data(fname, hasher):
+    dfx = pd.read_csv(fname)
+    sentence_list = [sent for sent in dfx['clean_text'].values]
+    data = hasher.transform(sentence_list)
+    return data
+    pass
+
+
+def main():
+    trainfile = '~/Data/Kaggle/real_or_not/clean_train.csv'
+    testfile = '~/Data/Kaggle/real_or_not/clean_test.csv'
+
+    hasher = get_hash_vectorizer(trainfile, n=30)
+
+    train_hash = get_hash_data(trainfile, hasher)
+    test_hash = get_hash_data(testfile, hasher)
+
+    # save the hash
+    np.save('train_data.npy', train_hash)
+    np.save('test_data.npy', test_hash)
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ real_or_not/logistic_model.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+
+
+def get_model():
+    pass
+
+
+def train(model, x, y):
+    pass
+
+
+def score(model, x_t):
+    pass
+
+
+def main():
+    # Get data
+
+    # Train model
+
+    # Score model
+
+    # Save scores
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
