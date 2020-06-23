4119c1c713f861302b0ba7b23ea7f67772297325
Sachin Umrao <sachin.umrao.1512@gmail.com>
2020-06-24 03:13:17 +0530

    built logistic model'

--- real_or_not/hashing_vectorizer.py
+++ real_or_not/hashing_vectorizer.py
@@ -16,22 +16,21 @@ def get_hash_data(fname, hasher):
     dfx = pd.read_csv(fname)
     sentence_list = [sent for sent in dfx['clean_text'].values]
     data = hasher.transform(sentence_list)
-    return data
-    pass
+    return data.toarray()
 
 
 def main():
     trainfile = '~/Data/Kaggle/real_or_not/clean_train.csv'
     testfile = '~/Data/Kaggle/real_or_not/clean_test.csv'
 
-    hasher = get_hash_vectorizer(trainfile, n=30)
+    hasher = get_hash_vectorizer(trainfile, n=64)
 
     train_hash = get_hash_data(trainfile, hasher)
     test_hash = get_hash_data(testfile, hasher)
 
     # save the hash
-    np.save('train_data.npy', train_hash)
-    np.save('test_data.npy', test_hash)
+    np.save('train_data_64.npy', train_hash)
+    np.save('test_data_64.npy', test_hash)
     pass
 
 
--- real_or_not/logistic_model.py
+++ real_or_not/logistic_model.py
@@ -4,26 +4,42 @@ from sklearn.linear_model import LogisticRegression
 
 
 def get_model():
-    pass
-
-
-def train(model, x, y):
-    pass
-
-
-def score(model, x_t):
+    model = LogisticRegression(penalty='l2',
+                               random_state=42,
+                               max_iter=1000,
+                               verbose=2,
+                               n_jobs=-1)
+    return model
     pass
 
 
 def main():
     # Get data
+    train_csv = '~/Data/Kaggle/real_or_not/train.csv'
+    subm_csv = '~/Data/Kaggle/real_or_not/sample_submission.csv'
+
+    train_data_file = 'train_data_64.npy'
+    test_data_file = 'test_data_64.npy'
+
+    train_dfx = pd.read_csv(train_csv)
+    subm_dfx = pd.read_csv(subm_csv)
+
+    train_mat = np.load('train_data.npy', allow_pickle=True)
+    test_mat = np.load('test_data.npy', allow_pickle=True)
+
+    # Get the model
+    model = get_model()
 
     # Train model
+    model.fit(train_mat, train_dfx['target'].values)
 
     # Score model
+    y_hat = model.predict(test_mat)
+
+    subm_dfx['target'] = y_hat
 
     # Save scores
-
+    subm_dfx.to_csv('~/Data/Kaggle/real_or_not/submissions/subm_logistic_64.csv', index=False)
     pass
 
 
