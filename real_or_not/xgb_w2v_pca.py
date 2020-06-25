705d5f9f5c38590f28f615741b611cdfff36c38a
Sachin Umrao <sachin.umrao.1512@gmail.com>
2020-06-26 03:36:28 +0530

    added pca to word to vec

---
+++ real_or_not/xgb_w2v_pca.py
@@ -0,0 +1,89 @@
+import numpy as np
+import pandas as pd
+import xgboost as xgb
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.decomposition import PCA
+
+
+def get_model():
+    model = xgb.XGBClassifier(random_state=42)
+    return model
+    pass
+
+
+def main():
+    # Get data
+    train_csv = '~/Data/Kaggle/real_or_not/train.csv'
+    subm_csv = '~/Data/Kaggle/real_or_not/sample_submission.csv'
+
+    train_data_file = 'train_s2v_300.npy'
+    test_data_file = 'test_s2v_300.npy'
+
+    train_dfx = pd.read_csv(train_csv)
+    subm_dfx = pd.read_csv(subm_csv)
+
+    x_train = np.load(train_data_file, allow_pickle=True)
+    x_test = np.load(test_data_file, allow_pickle=True)
+
+    y_train = train_dfx['target'].values
+
+    # Reduce the dimensions of data
+    pca = PCA(n_components=100, random_state=1)
+    pca.fit(x_train)
+    x_train_t = pca.transform(x_train)
+    x_test_t = pca.transform(x_test)
+    # Get model
+    model = get_model()
+
+    # Parameter dictionary for hyper-parameter tuning
+    params = {
+        'eta': [0.01, 0.03, 0.05],
+        'min_child_weight': [0.6, 0.8, 1.0],
+        'max_depth': [8, 10, 12],
+        'max_leaf_node': [4, 6, 8],
+        'gamma': [0.0, 0.1, 0.2],
+        'subsample': [0.7, 0.8, 0.8],
+        'colsample_bytree': [0.8, 1.0],
+        'lambda': [0.5, 1.0, 1.5],
+        'alpha': [0.5, 0.1],
+        'n_estimators': [800, 1000, 1200],
+    }
+
+    # Train model
+    cross_val = RepeatedStratifiedKFold(n_splits=10,
+                                        n_repeats=2,
+                                        random_state=42)
+
+    random_cv = RandomizedSearchCV(estimator=model,
+                                   param_distributions=params,
+                                   n_iter=10,
+                                   n_jobs=-1,
+                                   cv=cross_val,
+                                   scoring='roc_auc',
+                                   refit=True,
+                                   verbose=1,
+                                   return_train_score=True)
+
+    random_cv.fit(x_train_t, y_train)
+
+    # Save hyper parameter tuning results
+    cv_df = pd.DataFrame.from_dict(random_cv.cv_results_)
+    cv_df.to_csv('~/Data/Kaggle/real_or_not/cross_val_summary_2_word2vec_pca.csv', index=False)
+
+    # Print best params
+    print("Best Params: ")
+    print(random_cv.best_params_)
+
+    # Score the model
+    best_model = random_cv.best_estimator_
+    y_preds = best_model.predict(x_test_t)
+    subm_dfx['target'] = y_preds
+
+    # Save scores
+    subm_dfx.to_csv('~/Data/Kaggle/real_or_not/submissions/subm_xgb_w2v300_pca.csv', index=False)
+    pass
+
+
+if __name__ == "__main__":
+    main()
