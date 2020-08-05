cd4711a0064a7fcb6faa67e5bfa8b61f5d6cb77a
Sachin Umrao <sachin.umrao.1512@gmail.com>
2020-08-06 05:11:56 +0530

    Added dataset module

---
+++ APTOS/config.py
@@ -0,0 +1,3 @@
+TRAIN_CSV_FILE = '~/Data/Kaggle//aptos2019-blindness-detection/train.csv'
+TRAIN_IMG_FOLDER = '~/Data/Kaggle//aptos2019-blindness-detection/train_images/'
+IMG_SIZE = 256
---
+++ APTOS/dataset.py
@@ -0,0 +1,48 @@
+import torch 
+from torchvision import transforms
+from PIL import Image
+import numpy as np
+import pandas as pd
+from torchvision.transforms.transforms import Normalize, Resize, ToTensor
+import config 
+
+class AptosDataset:
+
+    def __init__(self):
+        self.train_data = pd.read_csv(config.TRAIN_CSV_FILE)
+        self.train_folder = config.TRAIN_IMG_FOLDER
+        self.img_transform = transforms.Compose([
+                                transforms.Resize(config.IMG_SIZE),
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+
+    def __len__(self):
+        N = self.train_data.shape[0]
+        return N
+
+    def __getitem__(self, idx):
+        y = self.train_data['diagnosis'].iloc[idx]
+        img_name = self.train_data['id_code'].iloc[idx]
+        x = self.get_image(img_name)
+        x = self.img_transform(x)
+        y = torch.tensor(y)
+
+        data = {
+            'x': x,
+            'y': y
+        }
+        return data
+
+    def get_image(img_name):
+        img_path = config.TRAIN_IMG_FOLDER + img_name + '.png'
+        img = Image(img_path)
+        return img
+
+
+if __name__ == "__main__":
+    idx = 10
+    data = AptosDataset()
+    x, y = data.__getitem__(idx)
+    print(x.shape)
+    print(y)
